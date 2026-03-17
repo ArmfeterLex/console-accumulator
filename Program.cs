a11a86c75ead553ce1f05daa3e06c9e1572f4214
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-17 21:16:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace con124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0, sum = 0;
+            string s = "";
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                s = Console.ReadLine(); 
+
+                if (s == "Q") break; 
+
+                if (double.TryParse(s, out n))
+                {
+                    sum += n; 
+                    Console.WriteLine("сумма = " + sum);
+                }
+            }
+        }
+    }
+}
